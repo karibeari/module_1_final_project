5947bf83d51f239e23b1709997b1f3bfb692559e
github username <github email address>
2019-03-28 23:16:43 -0600

    add messages for nil methods

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :lists
   has_many :trails, through: :lists
 
+
   def add_to_wish_list(trail_id)
     if List.find_by(trail_id: trail_id, user: self, completed: false)
         puts "\nThis trail is already on your wishlist\n"
@@ -12,7 +13,7 @@ class User < ActiveRecord::Base
   end
 
 
-  def add_to_done_list(trail_id) #trail_id shoul be string
+  def add_to_done_list(trail_id)
     self.lists.each do |list|
       if list.trail_id == trail_id
         List.update(list.id, :completed => true)
@@ -20,26 +21,32 @@ class User < ActiveRecord::Base
     end
   end
 
-  def completed#shows trails completed
-     List.all.where(user: self, completed: true)
 
+  def completed
+     List.all.where(user: self, completed: true)
   end
 
-  def wish_list#shows trails on wish list
+
+  def wish_list
     List.all.where(user: self, completed: false)
   end
 
-  def print_trails_from_list(lists)#helper method
+
+  def print_trails_from_list(lists)
     lists.map{|list| list.trail}.map{|trail|puts "\n#{trail.id} - #{trail.name}\n"}
   end
+
 
   def my_location
     self.location
   end
 
+
   def find_trail_by_city
-    Trail.all.where(city: my_location)
+    city_trails = Trail.all.where(city: my_location)
+    city_trails.empty? ? (puts "Sorry, there are no trails in your city.") : city_trails
   end
+
 
   def find_buddies
     buddies = []
@@ -49,12 +56,16 @@ class User < ActiveRecord::Base
         buddies << {match.user.name => match.trail.name}
       end
     end
+    puts "Sorry, you do not share any trails on your wish list with other hikers.  Try adding more trails to your wish list." if buddies.empty?
+
     buddies
   end
+
 
   def miles_hiked
     self.completed.map{|list| list.trail.length}.sum
   end
+
 
   def format_trails(trails_array)
     trails_array.each do |trail|
@@ -65,6 +76,7 @@ class User < ActiveRecord::Base
     end
   end
 
+
   def print_user_profile
     puts "\nName: #{self.name}\n"
     puts "\nAge: #{self.age}\n"
@@ -72,6 +84,5 @@ class User < ActiveRecord::Base
     puts "\nExperience Level: #{self.experience_level}\n"
     puts "\nProfile: #{self.profile}\n"
   end
-
 
 end
